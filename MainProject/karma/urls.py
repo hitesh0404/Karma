788419d37ca34d0fd209544340140b9495ddffd7
hitesh <hiteshkukaramprajapati@gmail.com>
2024-09-05 13:05:03 +0530

    UI ready

--- MainProject/karma/urls.py
+++ MainProject/karma/urls.py
@@ -21,4 +21,20 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',TemplateView.as_view(template_name='index.html')),
 
+
+
+    path('login/',TemplateView.as_view(template_name='login.html'),name="login"),
+    path('tracking/',TemplateView.as_view(template_name='tracking.html'),name="tracking"),
+    path('elements/',TemplateView.as_view(template_name='elements.html'),name="elements"),
+    path('contact/',TemplateView.as_view(template_name='contact.html'),name="contact"),
+    path('index/',TemplateView.as_view(template_name='index.html'),name="index"),
+    path('category/',TemplateView.as_view(template_name='category.html'),name="category"),
+    path('single-product/',TemplateView.as_view(template_name='single-product.html'),name="single-product"),
+    path('checkout/',TemplateView.as_view(template_name='checkout.html'),name="checkout"),
+    path('cart/',TemplateView.as_view(template_name='cart.html'),name="cart"),
+    path('confirmation/',TemplateView.as_view(template_name='confirmation.html'),name="confirmation"),
+    path('blog/',TemplateView.as_view(template_name='blog.html'),name="blog"),
+    path('single-blog/',TemplateView.as_view(template_name='single-blog.html'),name="single-blog"),
+
+
 ]
