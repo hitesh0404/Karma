21f31c3cdc3e192e0572fcaaa8334c47ce990a1a
hitesh <hiteshkukaramprajapati@gmail.com>
2024-09-16 16:03:19 +0530

    Python ORM queries and one sample steps to insert data in newly created model

--- MainProject/product/models.py
+++ MainProject/product/models.py
@@ -9,3 +9,15 @@ class Product(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=25)
+
+class ProductType(models.Model):
+    id  = models.IntegerField(models.BigAutoField,primary_key=True,auto_created=True)
+    name = models.CharField(max_length=25)
+    description = models.TextField(verbose_name='desc')
+    def __str__(self)->str:
+        return self.name
+
+# python manage.py makemigrations
+# python manage.py migrate
+# # for i in Product.objects.filter(price__in = ( 176.43, 250.60,176.44,150)):
+#     print(f'{str(i.id).center(4,' ')} | {i.name.center(20,' ' )} | {i.price}')
