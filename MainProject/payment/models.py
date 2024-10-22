67ebd7778b86092bd4d08780544e22eef1f1a573
hitesh <hiteshkukaramprajapati@gmail.com>
2024-10-22 14:37:25 +0530

    Payment verification view completed

--- MainProject/payment/models.py
+++ MainProject/payment/models.py
@@ -15,7 +15,9 @@ METHOD_CHOICE = [
 ]
 class Payment(models.Model):
     user=models.ForeignKey(User,on_delete=models.DO_NOTHING)
-    payment_signature=models.CharField(max_length=128,default='default')
+    razorpay_order_id = models.CharField(max_length=25,blank=True,default="default")
+    razorpay_payment_id = models.CharField(max_length=25,blank=True,default="default")
+    payment_signature=models.CharField(max_length=128,default='default',blank=True)
     amount=models.DecimalField(decimal_places=2,max_digits=12) 
     status=models.CharField(choices=STATUS_CHOICE,max_length=25)
     method=models.CharField(choices=METHOD_CHOICE,max_length=25)
--- MainProject/payment/views.py
+++ MainProject/payment/views.py
@@ -7,8 +7,7 @@ from django.contrib.auth.models import User
 from cart.models import Cart
 from order.models import Order,OrderDetails
 from .models import Payment
-from accounts.models import Customer
-from accounts.models import Address
+from accounts.models import Customer,Address
 from django.views.decorators.csrf import csrf_exempt
 
 def checkout(request):
@@ -45,6 +44,15 @@ def payment(request):
         'payment':payment,
         'callback_url':'/payment/verify-payment/'
     }
+    print('\n\n\n\n\n\n',payment,type(payment),'\n\n\n\n\n\n')
+    Payment.objects.create(
+            user=user,
+            razorpay_order_id = payment['id'],
+            amount = order_obj.total/100,
+            status = 'PENDING',
+            method = 'RAZORPAY',
+            order = order_obj
+        )
     data = dumps(context,indent=4)
     print(type(data),data,order_obj.__dict__)
     return HttpResponse(data)
@@ -53,23 +61,26 @@ def payment(request):
 def verify_payment(request):
     try:
         client = razorpay.Client(auth=(settings.RAZORPAY_KEY_ID,settings.RAZORPAY_KEY_SECRET ))
+        pay ={                                  'razorpay_order_id': request.POST.get('razorpay_order_id'),
+                                                'razorpay_payment_id': request.POST.get('razorpay_payment_id'),
+                                                'razorpay_signature': request.POST.get('razorpay_signature'),
+                                                }
+        print(pay)
         client.utility.verify_payment_signature({
                                                 'razorpay_order_id': request.POST.get('razorpay_order_id'),
                                                 'razorpay_payment_id': request.POST.get('razorpay_payment_id'),
                                                 'razorpay_signature': request.POST.get('razorpay_signature'),
                                                 })
+        
         print('done succesful Payment')
-        user  = get_object_or_404(User,username=request.user)
-        cart = Cart.objects.filter(user = user)
-        order_obj = Order.objects.get(request.POST.get('razorpay_payment_id'))
-        Payment.objects.create(
-            user=user,
-            payment_signature = request.POST.get('razorpay_signature'),
-            amount = order_obj.total/100,
-            status = 'COMPLETED',
-            method = 'RAZORPAY',
-            order = order_obj
-        )
+        payment_obj = Payment.objects.get(razorpay_order_id =str(request.POST.get('razorpay_order_id')))
+        cart = Cart.objects.filter(user = payment_obj.user)
+        print(cart)
+        order_obj = payment_obj.order
+        payment_obj.razorpay_payment_id = str(request.POST.get('razorpay_payment_id'))
+        payment_obj.payment_signature = str( request.POST.get('razorpay_signature'))
+        payment_obj.status = "COMPLETED"
+        payment_obj.save()
         for item in cart:
             OrderDetails.objects.create(
                 order_id = order_obj,
@@ -78,7 +89,6 @@ def verify_payment(request):
                 price = item.product.price_inclusive
             )
         cart.delete()
-
         return redirect('/')
     except Exception as e:
         print(e)
