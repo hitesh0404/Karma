6fbe57b307fd929085e147b6161c1302f8003e9f
hitesh <hiteshkukaramprajapati@gmail.com>
2024-10-25 19:14:59 +0530

    Filter

--- MainProject/products/urls.py
+++ MainProject/products/urls.py
@@ -5,5 +5,6 @@ urlpatterns= [
     path('update-product/<int:id>/',views.UpdateProduct.as_view(),name = 'update_product'),
     path('show-product-list/',views.show_product,name = 'product_list'),
     path('show-product-list/sort-by-category/<str:name>/',views.sort_by_category,name = 'sort_by_category'),
+    path('show-product-list/sort-by-price/<str:name>/',views.sort_by_price,name = 'sort_by_price'),
 
 ]
--- MainProject/products/views.py
+++ MainProject/products/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect,get_object_or_404
 from django.views import View
 from .forms import ProductForm
 from .models import Product,ShoeCategory,Shoe
-
+from django.db.models import Q
 class AddProduct(View):
     def get(self,request):
         form = ProductForm()
@@ -33,9 +33,19 @@ class UpdateProduct(View):
             # form = ProductForm(request.POST)
             return render(request,'products/update_product.html',{'form':form})
 
+def search_products(search):
+    products = Product.objects.filter(name__icontains = search)
+    if not products.exists():
+        products = Product.objects.filter(description__icontains = search)
+    return products
+
 
 def show_product(request):
-    products = Product.objects.all()
+    search = request.GET.get('search')
+    if search:
+        products = search_products(search)
+    else:
+        products = Product.objects.all()
     shoe_category = ShoeCategory.objects.all() 
     context ={
         'products':products,
@@ -49,6 +59,23 @@ def sort_by_category(request,name):
     shoecategoryshoe = cat.shoecategoryshoe_set.all()
     products = [product.shoe.product for product in shoecategoryshoe]
     print(products)
+    shoe_category = ShoeCategory.objects.all() 
+    context ={
+        'products':products,
+        'category':shoe_category,
+    }
+    return render(request,'products/show_product.html',context)
+
+def sort_by_price(request,name):
+    greater_then,less_then = name.split('&')
+    products = Product.objects.filter(
+        Q(price_inclusive__lt =int(less_then)) 
+            &
+        Q(price_inclusive__gt=int(greater_then))
+         
+        )
+    
+
     shoe_category = ShoeCategory.objects.all() 
     context ={
         'products':products,
